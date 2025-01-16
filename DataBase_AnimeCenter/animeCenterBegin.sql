0ecc859e39462b3c8b2cb2db727b608759b20944
HaiMinhDoan <haiminh17052003@gmail.com>
2025-01-16 18:47:38 +0700

    Hehehe

---
+++ DataBase_AnimeCenter/animeCenterBegin.sql
@@ -0,0 +1,113 @@
+create schema animecenter;
+use animecenter;
+INSERT INTO genres (name) VALUES 
+('Adventure'),
+('Action'),
+('Drama'),
+('Fantasy'),
+('Thriller'),
+('Horror'),
+('Biography'),
+('Sci-Fi'),
+('Romance');
+
+INSERT INTO movies (title, japanese_title, description, duration, quality, total_views, image, film_link) VALUES
+('Flow', 'Straume', 'Ở một thế giới hoang tàn vắng bóng sự hiện diện của con người, sau khi mái ấm của một chú mèo bị lũ cuốn trôi, chú ta bắt đầu một cuộc phiêu lưu vĩ đại trên một chiếc thuyền cùng với những loài động vật khác.', 85, 'HD', 100, 'https://th.bing.com/th/id/OIP.Hlzvpz7ilJ9hcKWnda4NQAHaLH?rs=1&pid=ImgDetMain', 'https://embed14.streamc.xyz/embed.php?hash=5c62cc9d8e2d6440918e12f4fcf03dab'),
+
+('Nhim Sonic 3', 'Sonic the Hedgehog 3', 'Sonic, Knuckles và Tails phải đối mặt với một kẻ thù mới cực kỳ mạnh mẽ là Shadow.', 110, 'HD', 100, 'https://lh4.googleusercontent.com/-WkpcxYLy99I/UPrLyf-g9eI/AAAAAAAAkjk/R3B1l1EJpZI/s700/130120_sonic01.jpg', 'https://embed11.streamc.xyz/embed.php?hash=91fa7b210b79508999449367309968f3'),
+
+('Anh hùng Gunnar Sonsteby', 'nr.24', 'Gunnar Sonsteby, một thanh niên tham gia cuộc kháng chiến chống lại Đức Quốc xã.', 112, 'HD', 100, 'https://th.bing.com/th/id/OIP.UlPxUIvfuWV60z3QlYq9LQHaDR?rs=1&pid=ImgDetMain', 'https://embed13.streamc.xyz/embed.php?hash=be29e04a65c58197ae675b6c368bed87'),
+
+('Zim Zim and Kazim Magic', 'Zim Zim Ala Kazim', 'Khi các thế lực tà ác truy sát người thầy đáng kính Ustaz Kazim Elias.', 100, 'HD', 100, 'https://th.bing.com/th/id/OIP.lls4DL4LQfjJ4Cxf9s_1-QHaJQ?rs=1&pid=ImgDetMain', 'https://embed11.streamc.xyz/embed.php?hash=356ca6b873c15d4659528d3aa1b8e111'),
+
+('The Hidden Devil', 'When Evil Lurks', 'Tại một ngôi làng hẻo lánh, hai anh em tìm thấy một người con dê bị nguyền.', 100, 'HD', 100, 'https://th.bing.com/th/id/OIP.C7zOkV3uOnFvF0LhQxMAYQAAAA?rs=1&pid=ImgDetMain', 'https://embed11.streamc.xyz/embed.php?hash=4e3d1c4751834a4ca0c84c47ec44e6f9'),
+
+('Hidden Fortress', 'The Hidden Fortress', 'Pháo đài ẩn mang trong mình cốt truyện kéo dài từ thời Heian đến thời Edo.', 139, 'HD', 100, 'https://th.bing.com/th/id/OIP.OsE-6hHuCUqW2ldZrv5-TQAAAA?rs=1&pid=ImgDetMain', 'https://embed12.streamc.xyz/embed.php?hash=8935b9d36ffda8bfbf30912d05a8fb5e'),
+
+('Ad Vitam', 'Ad Vitam', 'Khi anh và người vợ đang mang thai bị tấn công tại nhà.', 97, 'HD', 100, 'https://phimmoida.org/wp-content/uploads/2025/01/thumb-107815-107815-300x450.jpg', 'https://embed13.streamc.xyz/embed.php?hash=af93d1a1fcf853c1f48a50e74c415bc2'),
+
+('Drug Boss 3', 'Pusher 3', 'Milo cố gắng trở thành một người đàn ông của gia đình và điều hành tổ chức tội phạm của mình.', 108, 'HD', 100, 'https://th.bing.com/th?id=OIF.IhSHVilCPlfNhro%2bZfkdOg&rs=1&pid=ImgDetMain', 'https://embed17.streamc.xyz/embed.php?hash=22422b027a874f7eeb9f12537986f3bf'),
+
+('The Elephant Sat Still on the Ground', 'An Elephant Sitting Still', 'Ở thành phố Manzhouli, miền bắc Trung Quốc, họ nói rằng có một con voi chỉ đơn giản là ngồi và phớt lờ thế giới.', 234, 'HD', 100, 'https://i1.wp.com/aftercredits.com/wp-content/uploads/2019/03/AnElephantSittingStillPoster.jpg', 'https://embed14.streamc.xyz/embed.php?hash=ab652742446ddafc2ed3f207685aedf2'),
+
+('Drug Lord', 'Pusher', 'Frank là một kẻ buôn bán ma túy cho đến khi anh ta thực hiện một giao dịch lớn.', 110, 'HD', 100, 'https://img.ahaphimz.com/movies/content/m.preview.aha.8fRLUji.jpg', 'https://embed12.streamc.xyz/embed.php?hash=8bdc7aaa7f44261748129758a8d6f0a0'),
+
+('Vampire Queen', 'Queen of the Damned', 'Ma cà rồng Lestat trở thành một ngôi sao nhạc rock, sử dụng âm nhạc để đánh thức nữ hoàng của mọi ma cà rồng.', 101, 'HD', 100, 'https://th.bing.com/th/id/OIP.z4fBjK0E6Y152U5jdQbxewHaLK?rs=1&pid=ImgDetMain', 'https://embed11.streamc.xyz/embed.php?hash=eb16b7fde70edbe62c1f56a19c32f24e'),
+
+('The Lady of the Green Forest', 'Sheena', 'Sheena cai trị vùng đất hoang dã của châu Phi, bảo vệ rừng già và các loài vật.', 117, 'HD', 100, 'https://th.bing.com/th/id/OIP.5kQpvqApN7_wGhjAHnu4wwHaKj?rs=1&pid=ImgDetMain', 'https://embed13.streamc.xyz/embed.php?hash=d9d9d6f6b9f3fdd8deb473e4443f6f77'),
+
+('The End of the Assassin', 'Sniper: Assassin\'s End', 'Bị đổ oan tội ám sát, một quân nhân đặc nhiệm nhờ bố mình – một tay súng bắn tỉa huyền thoại.', 95, 'HD', 100, 'https://motchillt.com/storage/images/hoi-ket-cua-sat-thu/5VXI9NkugrLHJTSrGE4X8UVzKaX.jpg', 'https://embed15.streamc.xyz/embed.php?hash=5ce157c91d6c58e59fa6277f72f1b117'),
+
+('Cold Mountain Town', 'Cold Mountain', 'Trong câu chuyện kinh điển về tình yêu và sự tận tụy này lấy bối cảnh là Nội chiến Hoa Kỳ.', 154, 'HD', 100, 'https://th.bing.com/th/id/R.35582c707e3ecf5e17f259d7e85a5645?rik=PNZ0taZuE0vhJA&pid=ImgRaw&r=0', 'https://embed14.streamc.xyz/embed.php?hash=6aa204a7326ab793a97f86d8f083ec87'),
+
+('Waiting for Love', 'Anna Magdalena', 'Hai thanh niên phải lòng người phụ nữ ở tầng trên khi cô ấy tập bản J.S. "Sổ tay cho Anna Magdalena" của Bach.', 97, 'HD', 100, 'https://th.bing.com/th/id/R.4a448fab6bcc6cac424d7271bf8133fe?rik=Q54BefGZpsiRKQ&pid=ImgRaw&r=0', 'https://embed12.streamc.xyz/embed.php?hash=ae74f149a07cefc993e323ad004d84ee'),
+
+('Life and Death Game', 'The Death Game', 'Truyền thuyết giang hồ "Thử thách Ngũ Độc" là một trò chơi sinh tử, chỉ có một người sống sót có thể giành được phần thưởng.', 88, 'HD', 100, 'https://th.bing.com/th/id/OIP.3z8pS0cRqy5Z5TpXHOULIwHaFM?rs=1&pid=ImgDetMain', 'https://embed13.streamc.xyz/embed.php?hash=5436f14b998b0b45b75c77ea8f125b05');
+
+INSERT INTO movie_genres (movie_id, genre_id) VALUES
+-- Flow: Adventure, Fantasy
+(1, 1),
+(1, 4),
+
+-- Nhim Sonic 3: Adventure, Action, Fantasy
+(2, 1),
+(2, 2),
+(2, 4),
+
+-- Anh hùng Gunnar Sonsteby: Biography, Action, Drama
+(3, 7),
+(3, 2),
+(3, 3),
+
+-- Zim Zim and Kazim Magic: Adventure, Fantasy, Thriller
+(4, 1),
+(4, 4),
+(4, 5),
+
+-- The Hidden Devil: Thriller, Horror
+(5, 5),
+(5, 6),
+
+-- Hidden Fortress: Adventure, Drama
+(6, 1),
+(6, 3),
+
+-- Ad Vitam: Action, Thriller
+(7, 2),
+(7, 5),
+
+-- Drug Boss 3: Drama, Thriller
+(8, 3),
+(8, 5),
+
+-- The Elephant Sat Still on the Ground: Drama
+(9, 3),
+
+-- Drug Lord: Action, Thriller
+(10, 2),
+(10, 5),
+
+-- Vampire Queen: Horror, Fantasy
+(11, 6),
+(11, 4),
+
+-- The Lady of the Green Forest: Adventure, Fantasy, Drama
+(12, 1),
+(12, 4),
+(12, 3),
+
+-- The End of the Assassin: Action, Thriller
+(13, 2),
+(13, 5),
+
+-- Cold Mountain Town: Drama, Romance
+(14, 3),
+(14, 9),
+
+-- Waiting for Love: Romance, Drama
+(15, 9),
+(15, 3),
+
+-- Life and Death Game: Action, Thriller
+(16, 2),
+(16, 5);
